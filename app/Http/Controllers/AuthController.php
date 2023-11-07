45b12ef52455a45d5c6a463b523353c3712a3efc
Malek Cherif <cherifmalekk@gmail.com>
2023-11-07 14:11:58 +0100

    API authentication

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+
+class AuthController extends Controller
+{
+    public function login(Request $request): JsonResponse
+    {
+        $request->validate([
+            'email' => 'required|email',
+            'password' => 'required',
+        ]);
+
+        $user = User::where('email', $request->email)->first();
+        if (!$user || !Hash::check($request->password, $user->password)) {
+            return response()->json([
+                'error' => 'The provided credentials are incorrect.',
+            ], 401);
+        }
+
+        return response()->json([
+            'api_token' => $user->createToken('api_token')->plainTextToken,
+        ]);
+    }
+}
